e1c5639cb1b3559d07119b9aaecc5af023f28da4
Emil Broman <emil@emilbroman.me>
2023-08-25 16:02:50 +0200

    Upgrade dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    // @ts-expect-error because @vitejs/plugin-react doesn't use .js import extensions as of 2022-11-08
-    react(),
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": { target: { port: 8126, host: "localhost" } },
